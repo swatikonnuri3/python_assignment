e6286b326a8a4faa0bba3a08b9f1769ad5941690
swati konnuri <swatikonnuri@gmail.com>
2026-02-19 09:23:29 +0530

    Completed string_formatting assignment

---
+++ src/string_formatting/driver.py
@@ -0,0 +1,5 @@
+from util import print_formatted
+
+if __name__ == '__main__':
+    n = int(input("Enter a Number: "))
+    print_formatted(n)
---
+++ src/string_formatting/util.py
@@ -0,0 +1,5 @@
+def print_formatted(number):
+    width = len(bin(number)) - 2
+    for i in range(1, number+1):
+        print(
+            f"{str(i).rjust(width)} {oct(i)[2:].rjust(width)} {hex(i)[2:].upper().rjust(width)} {bin(i)[2:].rjust(width)}")
