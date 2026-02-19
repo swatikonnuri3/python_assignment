373ce528701b398507a9dd8a6ea3ea7a5bee86b6
swati konnuri <swatikonnuri@gmail.com>
2026-02-19 09:21:02 +0530

    completed floor ceil rint assignment

---
+++ src/floor_ceil_rint/driver.py
@@ -0,0 +1,5 @@
+from util import print_fcr
+import numpy
+if __name__=="__main__":
+    A = numpy.array(input("Enter Array: ").split(), float)
+    print_fcr(A)
---
+++ src/floor_ceil_rint/util.py
@@ -0,0 +1,6 @@
+import numpy
+numpy.set_printoptions(sign=' ')
+def print_fcr(A):
+    print(numpy.floor(A))
+    print(numpy.ceil(A))
+    print(numpy.rint(A))
